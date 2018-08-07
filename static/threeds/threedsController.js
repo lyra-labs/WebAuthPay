4db7f62852b3e462336aea77129a2800e4da5d26
Maxime Bel <maxime.bel@epitech.eu>
2018-08-07 11:10:12 +0200

    last update for 3ds method

--- static/threeds/threedsController.js
+++ static/threeds/threedsController.js
@@ -35,44 +35,17 @@ let defaults = {
 
 // CReq content hardcoded
 let CReq = {
-    "threeDSServerTransID": "8a880dc0-d2d2-4067-bcb1-b08d1690b26e",
+    "threeDSServerTransID": "",
     "acsTransID": "d7c1ee99-9478-44a6-b1f2-391e29c6b340",
     "messageType": "CReq",
     "messageVersion": "2.1.0",
-    "sdkTransID": "b2385523-a66c-4907-ac3c-91848e8c0067",
-    "sdkCounterStoA": "001"
 }
 
 // will be used to close the auth Iframe later
 let savedIframe = {}
 
-// send the CReq and spawn the auth Iframe containing the plaintext HTML response
-let sendcReq = (acsURL, acsTransID, threeDSServerTransID) => {
-
-    CReq.acsTransID = acsTransID
-    CReq.threeDSServerTransID = threeDSServerTransID
-
-    fetch(acsURL, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(CReq)
-    })
-        .then((response) => response.text())
-        .then((response) => {
-
-            console.log(response);
-
-            savedIframe = $.featherlight(response, defaults)
-
-        })
-        .catch((error) => alert(error))
-}
-
-// send the form to the merchant server to initiate the transaction
-let startAuthentication = () => {
-    let paymentData = {
+let getPaymentData = () => {
+ return {
         cc_number: $('#cc-number-input').val(),
         email: $('#email-input').val(),
         cvv: $('#cvv-input').val(),
@@ -82,8 +55,36 @@ let startAuthentication = () => {
         postcode: $('#post-code-input').val(),
         city_name: $('#city-input').val(),
         phone_number: $('#tel-input').val(),
-        address: $('#address-input').val()
+        address: $('#address-input').val(),
+        // variable comes from threeDSMethod.js
+        threeDSServerTransID : threeDSServerTransID_global
     }
+}
+
+// send the CReq and spawn the auth Iframe containing the plaintext HTML response
+let sendcReq = (acsURL, acsTransID, threeDSServerTransID) => {
+    
+    CReq.acsTransID = acsTransID
+    CReq.threeDSServerTransID = threeDSServerTransID
+    fetch(acsURL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(CReq)
+    })
+        .then((response) => response.text())
+        .then((response) => {
+            console.log(response);
+            savedIframe = $.featherlight(response, defaults)
+        })
+        .catch((error) => alert(error))
+}
+
+// send the form to the merchant server to initiate the transaction
+let startAuthentication = () => {
+
+    let paymentData = getPaymentData()
 
     // assert that all inputs are filled
     $.each(paymentData, (i, value) => {
@@ -109,56 +110,7 @@ let startAuthentication = () => {
         })
 }
 
-// here we get the URL + 3dsServerTransID
-// We spawn the iframe (get ID, request wait response and message)
-// in the message handler we send the startPayment request
-
-
-$(document).ready(() => {
-
-    let getIframeContent = (threeDSServerTransID) => {
-        return fetch('http://localhost:4242/acs/getMethodHTML', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ "threeDSServerTransID": threeDSServerTransID })
-        })
-            .then((response) => response.text())
-            .then((response) => response)
-    }
-
-    let getThreeDSMethod_URL = () => {
-        return fetch('http://localhost:4242/merchant/init', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ cc_number: $('#cc-number-input').val() })
-        })
-            .then((response) => response.json())
-            .then((response) => response)
-    }
-
-    // make the request with the CC number to the merchant to get the ACS url of 3ds method
-    $('#button-iframe').click(() => {
-        getThreeDSMethod_URL()
-            .then((response) => {
-                if (response.status !== 'ok') {
-                    alert('Server error, your card may not be enrolled to 3DS2')
-                    return
-                } else {
-                    getIframeContent(response.threeDSServerTransID)
-                        .then((htmlContent) => {
-                            document.getElementById('methodIframe').contentDocument.write(htmlContent)
-                        })
-                }
-            })
-    })
-});
-
 // Recieve message from Auth Iframe and 3DSMethod_URL Iframe
-
 window.addEventListener("message", receiveMessage, false);
 
 function receiveMessage(event) {
@@ -170,23 +122,29 @@ function receiveMessage(event) {
     }
 
     if (event.data) {
+        if (event.data.messageType == 'CRes') {  // classic auth case (CReq/Cres)
+            NOTIFICATION_URL = event.data.notificationURL
 
-        NOTIFICATION_URL = event.data.notificationURL
-
-        fetch(NOTIFICATION_URL, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(event.data)
-        })
-            .then((response) => response.json())
-            .then((response) => {
-                console.log(response);
-                window.setTimeout(function () { savedIframe.close(); }, 2400);
-
+            fetch(NOTIFICATION_URL, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(event.data)
             })
-            .catch((error) => console.log(error))
+                .then((response) => response.json())
+                .then((response) => {
+                    console.log(response)
+                    window.setTimeout(function () { savedIframe.close(); }, 2400);
+
+                })
+                .catch((error) => console.log(error))
+        }
+        else if (event.data.status) {  // 3DS method case
+            console.log("starting authentication");
+            
+            startAuthentication()
+        }
     }
 
 
